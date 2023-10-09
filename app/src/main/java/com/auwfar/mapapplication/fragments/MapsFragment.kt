a4ac44ddd80554454c6eb51e5815b68f5a86ff2e
Auwfar <achmad.auwfar@alodokter.com>
2023-10-09 11:28:09 +0700

    Implement maps functionality

--- app/src/main/java/com/auwfar/mapapplication/fragments/MapsFragment.kt
+++ app/src/main/java/com/auwfar/mapapplication/fragments/MapsFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.auwfar.mapapplication.R
 import com.auwfar.mapapplication.databinding.FragmentMapsBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), OnMapReadyCallback {
 
     private var binding: FragmentMapsBinding? = null
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +35,18 @@ class MapsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.fragment_map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val jakarta = LatLng(-6.200000, 106.816666)
+        mMap.addMarker(MarkerOptions()
+            .position(jakarta)
+            .title("Marker in Jakarta"))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(jakarta, 6f))
 
 
     }
