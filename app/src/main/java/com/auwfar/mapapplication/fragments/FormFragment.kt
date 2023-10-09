fec7af763efa2639c775c4224dde5f43f3381913
Auwfar <achmad.auwfar@alodokter.com>
2023-10-09 09:22:46 +0700

    initiate UI

---
+++ app/src/main/java/com/auwfar/mapapplication/fragments/FormFragment.kt
@@ -0,0 +1,33 @@
+package com.auwfar.mapapplication.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.auwfar.mapapplication.databinding.FragmentFormBinding
+
+class FormFragment : Fragment() {
+
+    private var binding: FragmentFormBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFormBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
